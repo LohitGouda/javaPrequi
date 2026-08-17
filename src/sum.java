a6d91afba22081b3c11140fef3d90901f5b964c4
lohit <lohithgoudalohithgouda@gmail.com>
2026-08-17 15:47:36 +0530

    sum

---
+++ src/sum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int sum = 1;
+
+        for (int i = 1; i <= n; i++) {
+            sum = sum * i;
+        }
+
+        System.out.println("The factorialof first n numbers is: " + sum);
+    }
+}
